616514f76bd3f8ad15ec2c54f3377d6bf6c63551
bsong <bsong97@gmail.com>
2013-07-08 14:06:18 +0800

    created MOL.py

---
+++ MOL.py
@@ -0,0 +1,12 @@
+# Method of Lines method 
+# to discretize all but one dimension
+# effectively turning PDEs into ODEs
+# and therefore ODESolver can be used to solve
+
+# Requirement:
+# PDE should be an Initial Value Problem (Cauchy problem) because ODE integrators are IVP
+# Thus, elliptic equations such as Laplace equation cannot be used
+
+class MOL:
+    pass
+
